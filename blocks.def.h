fbc06db9a8fe7b23c5600d4e735229423675791d
Omar Mohamed <omarcoptan9@gmail.com>
2024-02-10 12:43:58 +0200

    Add my scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    {"", "sb-mic",                                        0,      9},
+    {"", "sb-volume",                                     0,      10},
+    {"", "sb-memory",                                     10,     14},
+    {"", "sb-clock",                                      60,     1},
+    {"", "sb-battery",                                    5,      3},
+    {"", "sb-internet",                                   5,      4},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
